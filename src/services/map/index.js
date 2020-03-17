361a104a3e6a0efa4d5adcb523092f6f69359dcf
alinebossi <alinebossi@hotmail.com>
2020-03-17 13:47:15 -0300

    Configure Services

---
+++ src/services/map/index.js
@@ -0,0 +1,13 @@
+export default {
+    async getVehicles(route = '') {
+        if (route) {
+            return await fetch('http://webservices.nextbus.com/service/publicJSONFeed?command=vehicleLocations&a=sf-muni&r=' + route)
+                .then(response => response.json())
+                .then(layer => layer.vehicle);
+        }
+        return await fetch('http://webservices.nextbus.com/service/publicJSONFeed?command=vehicleLocations&a=sf-muni')
+            .then(response => response.json())
+            .then(layer => layer.vehicle);
+
+    },
+};
