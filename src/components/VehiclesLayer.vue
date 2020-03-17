c195720802d1af92e8976baf48c82b08cb2b181a
alinebossi <alinebossi@hotmail.com>
2020-03-16 21:05:50 -0300

    Improve method in Vehicle Component

--- src/components/VehiclesLayer.vue
+++ src/components/VehiclesLayer.vue
@@ -9,6 +9,10 @@
     name: 'VehiclesLayer',
     props: ['layerConfiguration'],
     methods: {
+      updatePath: function(routeURL) {
+        var urlVehicle = this.layerConfiguration.jsonPath + '&r=' + routeURL;
+        this.layerConfiguration.jsonPath = urlVehicle;
+      },
       drawVehicles: function() {
         // D3 Projection
         var projection = d3
@@ -35,19 +39,14 @@
               .attr('cx', data => projection([data.lon, data.lat])[0])
               .attr('cy', data => projection([data.lon, data.lat])[1])
               .attr('r', this.layerConfiguration.style.r)
-              .attr('fill', this.layerConfiguration.style.fill)
+              .attr('fill', this.layerConfiguration.style.fill);
           });
       },
     },
     watch: {
       $route(route) {
-        var urlVehicle =
-          'http://webservices.nextbus.com/service/publicJSONFeed?command=vehicleLocations&a=sf-muni&r=' +
-          route.params.routerTag;
-        
-        this.layerConfiguration.jsonPath = urlVehicle;
+        this.updatePath(route.params.routerTag);
         this.drawVehicles();
-
       },
     },
     mounted() {
